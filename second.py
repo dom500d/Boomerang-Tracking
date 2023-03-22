93c9b2cb28e645b9bf3fe6a609c9462fc7006ad5
Dominic Orlando <dorlando@ucsd.edu>
2023-03-21 22:35:27 -0700

    Dense optical flow tracks the boomerang quite well, now I want to work on just plotting the position of the boomerang by either a box or a point, overlayed on top of the video instead of the magnitude of velocity through collor on top of the video

---
+++ second.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture("videos/DJI_0024.mp4")
+ret, frame1 = cap.read()
+prvs = cv.cvtColor(frame1, cv.COLOR_BGR2GRAY)
+hsv = np.zeros_like(frame1)
+hsv[..., 1] = 255
+cv.namedWindow('Window', cv.WINDOW_NORMAL)
+while(1):
+    ret, frame2 = cap.read()
+    if not ret:
+        print('No frames grabbed!')
+        break
+    next = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
+    flow = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    mag, ang = cv.cartToPolar(flow[..., 0], flow[..., 1])
+    hsv[..., 0] = ang*180/np.pi/2
+    hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+    bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+   # cv.imshow('Window', bgr)
+    # Open a new window and displays the output frame
+    dense_flow = cv.addWeighted(frame2, 1, bgr, 2, 0)
+    cv.imshow('Window', dense_flow)
+    # Update previous frame
+
+    k = cv.waitKey(1) & 0xff
+    if k == 27:
+        break
+    elif k == ord('s'):
+        cv.imwrite('opticalfb.png', frame2)
+        cv.imwrite('opticalhsv.png', bgr)
+    prvs = next
+cv.destroyAllWindows()
